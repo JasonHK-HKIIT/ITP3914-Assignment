c82bc9ebcc6edc1877303506f0af8873de6841ae
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2024-09-30 10:29:35 +0000

    Added Gomoko board generation

--- app/src/main/java/dev/jasonhk/hkiit/itp3914/assignment/Gomoko.java
+++ app/src/main/java/dev/jasonhk/hkiit/itp3914/assignment/Gomoko.java
@@ -2,28 +2,31 @@ package dev.jasonhk.hkiit.itp3914.assignment;
 
 public class Gomoko
 {
+    private static final int BOARD_SIZE = 10;
+
     public static void main(String[] args)
     {
-        
+        var board = new GomokoBoard();
+        board.printBoard();
     }
 
-    private enum ChessPiece
+    private static enum GomokoPiece
     {
-        EMPTY('0', 0),
-        BLACK('1', -1),
-        WHITE('2', 1);
+        EMPTY("0", 0),
+        BLACK("1", -1),
+        WHITE("2", 1);
 
-        private final char piece;
+        private final String piece;
 
         private final int value;
 
-        ChessPiece(char piece, int value)
+        GomokoPiece(String piece, int value)
         {
             this.piece = piece;
             this.value = value;
         }
 
-        public char getPiece()
+        public String getPiece()
         {
             return piece;
         }
@@ -31,6 +34,43 @@ public class Gomoko
         public int getValue()
         {
             return value;
+        }
+    }
+
+    private static class GomokoBoard
+    {
+        private GomokoPiece[][] board = new GomokoPiece[BOARD_SIZE][BOARD_SIZE];
+
+        public GomokoBoard()
+        {
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                for (int y = 0; y < BOARD_SIZE; y++)
+                {
+                    board[x][y] = GomokoPiece.EMPTY;
+                }
+            }
+        }
+
+        public void printBoard()
+        {
+            for (int x = 0; x <= BOARD_SIZE; x++)
+            {
+                if (x < BOARD_SIZE)
+                {
+                    System.out.printf("%d |", x);
+                }
+                else
+                {
+                    System.out.printf("  +%s%n   ", "-".repeat(2 * BOARD_SIZE));
+                }
+
+                for (int y = 0; y < BOARD_SIZE; y++)
+                {
+                    System.out.printf(" %s", (x < BOARD_SIZE) ? board[x][y].getPiece() : Integer.toString(y));
+                    if (y == (BOARD_SIZE - 1)) { System.out.println(); }
+                }
+            }
         }
     }
 }
